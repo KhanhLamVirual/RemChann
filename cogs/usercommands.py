7991c179a0876eaf961ef38f5f705bc8726eece9
KhanhLamVirual <termux02052010@gmail.com>
2023-06-13 13:02:45 +0700

    Add Avatar Command!

--- cogs/usercommands.py
+++ cogs/usercommands.py
@@ -16,6 +16,21 @@ class UserCommands(commands.Cog):
         embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
         embed.set_thumbnail(url=ctx.author.avatar)
         await ctx.reply(embed = embed)
-
+    
+    @commands.command()
+    async def avatar(self, ctx, member:discord.Member=None):
+        embed = discord.Embed(title="Avatar", color=discord.Color.random())
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avaatr)
+        if member == None:
+            embed.add_field(name=f"{ctx.author.display_name} Avatar's ", value=None, inline=True)
+            embed.set_image(url=ctx.author.avatar)
+            embed.set_footer(text=f"Link Download:[Here]({ctx.author.avatar})")
+            embed.set_thumbnail(url=ctx.author.avatar)
+        else:
+            embed.add_field(name=f"{member.display_name} Avatar's ", value=None, inline=True)
+            embed.set_image(url=member.avatar)
+            embed.set_footer(text=f"Link Download:[Here]({member.avatar})")
+            embed.set_thumbnail(url=member.avatar)
+        await ctx.reply(embed = embed)
 def setup(bot):
     bot.add_cog(UserCommands(bot))
