fe6cab44709f04a712968e80b0f6c1614c3e7731
KhanhLamVirual <termux02052010@gmail.com>
2023-06-13 16:14:06 +0700

    Make Add (add role )command In admincommand Running With Role

---
+++ cogs/admincommand.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+from discord.ext.commands import *
+import discord
+
+class AdminCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Loaded:AdminCommand Loaded")
+
+    @commands.command()
+    @has_role("Owner")
+    async def add(self, ctx, role:discord.Role=None, member:discord.Member=None):
+        embed = discord.Embed(title="Add Role", color=discord.Color.random())
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
+        if role != None:
+            if member != None:
+                await member.add_roles(role)
+                embed.add_field(name="Success", value=f"Added Role {role} To {member.display_name}", inline=True)
+            else:
+                embed.add_field(name="Missing", value="Missing Role Or Member", inline=True)
+
+        await ctx.reply(embed=embed)
+
+def setup(bot):
+    bot.add_cog(AdminCommand(bot))
